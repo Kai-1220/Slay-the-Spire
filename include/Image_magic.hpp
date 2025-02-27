d24bdb73862c961887cfdf0bf7bc3bb9c23d9364
112590059 <t112590059@ntut.org.tw>
2025-02-27 23:09:51 +0800

    push_test2

---
+++ include/Image_magic.hpp
@@ -0,0 +1,18 @@
+#include<PTSD/include/Util/Image.hpp>
+
+class Image_magic
+{
+private:
+    /* data */
+public:
+    Image_magic(/* args */);
+    ~Image_magic();
+};
+
+Image_magic::Image_magic(/* args */)
+{
+}
+
+Image_magic::~Image_magic()
+{
+}
