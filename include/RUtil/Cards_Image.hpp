4f8de17a872dc68dbab6f93192f57e05ff699f1e
112590059 <t112590059@ntut.org.tw>
2025-03-01 16:56:38 +0800

    nothing

--- include/RUtil/Cards_Image.hpp
+++ include/RUtil/Cards_Image.hpp
@@ -1,11 +1,13 @@
-// #ifndef RUTIL_CARDS_IMAGE_HPP
-// #define RUTIL_CARDS_IMAGE_HPP
-// #include "RUtil/Image_magic.hpp"
-// namespace RUtil {
-// class Cards_Image:public RUtil::Image_magic{
-// public:
-//     Cards_Image
-// private:
-// };
-// }
-// #endif
+#ifndef RUTIL_CARDS_IMAGE_HPP
+#define RUTIL_CARDS_IMAGE_HPP
+
+#include "Core/Texture.hpp"
+
+namespace RUtil {
+class Cards_Image:public RUtil::Image_magic{
+public:
+    Cards_Image
+private:
+};
+}
+#endif
