b2139dbe82d0994dea3c790971aed9e777fef707
112590059 <t112590059@ntut.org.tw>
2025-03-01 17:11:06 +0800

    build-remove

--- include/RUtil/Cards_Image.hpp
+++ include/RUtil/Cards_Image.hpp
@@ -1,10 +1,14 @@
 #ifndef RUTIL_CARDS_IMAGE_HPP
 #define RUTIL_CARDS_IMAGE_HPP
 
+#include "pch.hpp" // IWYU pragma: export
+
+#include <glm/fwd.hpp>
+
 #include "Core/Texture.hpp"
 
 namespace RUtil {
-class Cards_Image:public RUtil::Image_magic{
+class Cards_Image{
 public:
     Cards_Image
 private:
