6598fc2e289b06fedf523cf50aec35cc4919a1c8
112590059 <t112590059@ntut.org.tw>
2025-03-02 02:43:46 +0800

    Card_Image_building

--- src/RUtil/Cards_Image.cpp
+++ src/RUtil/Cards_Image.cpp
@@ -1,21 +1,26 @@
-// #include "RUtil/Cards_Image.hpp"
-// #include "pch.hpp"
-// #define card1_path RESOURCE_DIR"/Image/cards/cards.png
-// #define card2_path RESOURCE_DIR"/Image/cards/cards2.png
-// #define card3_path RESOURCE_DIR"/Image/cards/cards3.png
-// #define card4_path RESOURCE_DIR"/Image/cards/cards4.png
-// #define card5_path RESOURCE_DIR"/Image/cards/cards5.png
-// namespace RUtil {
-// void RUtil::Cards_Image::InitProgram(){
-//     s_Program =
-//         std::make_unique<Core::Program>(PTSD_ASSETS_DIR "/shaders/Base.vert",
-//                                         PTSD_ASSETS_DIR "/shaders/Base.frag");
-//     s_Program->Bind();
+#include "RUtil/Cards_Image.hpp"
+#include "pch.hpp"
+const char * const card_paths[] = {
+    RESOURCE_DIR"/Image/cards/cards.png",
+    RESOURCE_DIR"/Image/cards/cards2.png",
+    RESOURCE_DIR"/Image/cards/cards3.png",
+    RESOURCE_DIR"/Image/cards/cards4.png",
+    RESOURCE_DIR"/Image/cards/cards5.png"
+};
+namespace RUtil {
+void RUtil::Cards_Image::InitProgram(){
+    s_Program =
+        std::make_unique<Core::Program>(PTSD_ASSETS_DIR "/shaders/Base.vert",
+                                        PTSD_ASSETS_DIR "/shaders/Base.frag");
+    s_Program->Bind();
 
-//     GLint location = glGetUniformLocation(s_Program->GetId(), "surface");
-//     glUniform1i(location, UNIFORM_SURFACE_LOCATION);
-// }
-// void RUtil::Cards_Image::Init_Card_Textures(){
-//     s_Card_Textures.push_back(std::make_unique<)
-// }
-// }
+    GLint location = glGetUniformLocation(s_Program->GetId(), "surface");
+    glUniform1i(location, UNIFORM_SURFACE_LOCATION);
+}
+void RUtil::Cards_Image::Init_Card_Textures(){
+    for(const char * it:card_paths){
+        auto card_surface=std::make_shared<SDL_Surface>(IMG_Load(it),SDL_FreeSurface);
+        s_Card_Textures.push_back(std::make_unique<Core::Texture>(card_surface->format,card_surface->w,card_surface->h,card_surface->pixels));
+    }
+}
+}
