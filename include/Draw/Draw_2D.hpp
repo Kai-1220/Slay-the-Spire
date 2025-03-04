ec5fbe57807966a46c5f0e79cce26705bf9df342
112590059 <t112590059@ntut.org.tw>
2025-03-04 21:28:51 +0800

    Draw_2D_building_check_point

--- include/Draw/Draw_2D.hpp
+++ include/Draw/Draw_2D.hpp
@@ -1,22 +1,51 @@
-// #ifndef DRAW_DRAW_2D_HPP
-// #define DRAW_DRAW_2D_HPP
-// namespace Draw {
-// // 
-// /**
-//  * @class Draw_2D
-//  * @brief Easier drawing
-//  * 
-//  * Reference from java gdx spritebatch
-//  * 
-//  * VAO + VBO + IBO
-//  */
-// class Draw_2D
-// {
-// public:
-//     Draw_2D();
-//     ~Draw_2D();
-// private:
-//     int size
-// };
-// }
-// #endif
+#ifndef DRAW_DRAW_2D_HPP
+#define DRAW_DRAW_2D_HPP
+#include "pch.hpp"
+#include "Core\Program.hpp"
+#include "Util\Color.hpp"
+namespace Draw {
+// 
+/**
+ * @class Draw_2D
+ * @brief Easier drawing
+ * 
+ * Reference from java gdx spritebatch
+ * 
+ * VAO + VBO + EBO
+ * 
+ * x y color u v
+ */
+class Draw_2D
+{
+public:
+    Draw_2D(const int size=1000,const std::shared_ptr<Core::Program> &program=nullptr);
+    ~Draw_2D();
+    Draw_2D(const Draw_2D &) = delete;
+    Draw_2D(Draw_2D &&) = delete;
+    Draw_2D &operator=(const Draw_2D &) = delete;
+    Draw_2D &operator=(Draw_2D &&)=delete;
+
+    void SetColor(int r,int g,int b,int a=255);
+    void SetColor(Util::Colors color);
+    void SetColor(Uint32 color);
+
+    // void begin();
+private:
+    GLuint  m_EBO_BufferId,//only have one EBO. 
+            m_ArrayId,//one VAO.
+            m_VBO_BufferId;//one VBO.
+            //VAO VBO EBO will die after draw_2d die.
+    std::shared_ptr<Core::Program> DefaultProgram=std::make_shared<Core::Program>(
+                                                RESOURCE_DIR "/shader/java/default/default.vert",
+                                                RESOURCE_DIR "/shader/java/default/default.frag");
+    std::shared_ptr<Core::Program> NowProgram;
+    
+    GLuint LastTexture=0;
+    int idx=0,max_len;
+    GLfloat color;
+    // glm::mat4 m_Projection(1.0F),m_Transform(1.0F),u_Combine(1.0F);
+    bool drawing=false;
+
+};
+}
+#endif
---
+++ src/Draw/Draw_2D.cpp
@@ -0,0 +1,67 @@
+#include "Draw/Draw_2D.hpp"
+#include "Util/Logger.hpp"
+namespace Draw {
+    Draw_2D::Draw_2D(const int size,const std::shared_ptr<Core::Program> &program){
+        int len;
+        if(size>4095){
+            LOG_WARN("Size is bigger than 4095, so set to 4095");
+            len=4095*6;
+            max_len=4095*20;
+        }else{
+            len=size*6;
+            max_len=size*20;
+        }
+        //4 pos.x 4 pos.y 4 color 4 u 4 v
+        //total 20
+        //Init member
+        NowProgram=program==nullptr?DefaultProgram:program;
+        SetColor(Util::Colors::WHITE);
+        //set idx
+        std::vector<GLushort> Indices(len);
+        GLushort j=0;
+        //0 1 2
+        //2 3 0
+        for(int i=0;i<len;i+=6,j+=(GLushort)4){
+            Indices[i]=j;
+            Indices[i+1]=j+(GLushort)1;
+            Indices[i+2]=j+(GLushort)2;
+            Indices[i+3]=j+(GLushort)2;
+            Indices[i+4]=j+(GLushort)3;
+            Indices[i+5]=j;
+        }
+        //gen all
+        glGenBuffers(1, &m_EBO_BufferId);
+        glGenBuffers(1, &m_VBO_BufferId);
+        glGenVertexArrays(1, &m_ArrayId);
+        //bind all
+        glBindVertexArray(m_ArrayId);
+        glBindBuffer(GL_ARRAY_BUFFER,m_VBO_BufferId);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_EBO_BufferId);
+        //set indices
+        //just need set once
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, static_cast<GLsizeiptr>(Indices.size() * sizeof(GLushort)), Indices.data(), GL_STATIC_DRAW);
+        //set vao, also jsut need once
+        // x(float) y(float) color(float) u(float) v(float)
+        //set pos
+        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)0);
+        glEnableVertexAttribArray(0);
+        //set color
+        //normalize//note:test what happend if not do this.//note:test what different between unsigned_byte and byte.
+        glVertexAttribPointer(1, 4, GL_UNSIGNED_BYTE, GL_TRUE, 5 * sizeof(GLfloat), (GLvoid*)(sizeof(GLfloat)*2));
+        glEnableVertexAttribArray(1);
+        //set uv
+        glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(sizeof(GLfloat)*3));
+        glEnableVertexAttribArray(2);
+    }
+    Draw_2D::~Draw_2D(){
+        glDeleteVertexArrays(1, &m_ArrayId);
+        glDeleteBuffers(1, &m_EBO_BufferId);
+        glDeleteBuffers(1, &m_VBO_BufferId);
+    }
+
+    void Draw_2D::SetColor(int r,int g,int b,int a){SetColor(Uint32(r<<24|g<<16|b<<8|a));}
+    void Draw_2D::SetColor(Util::Colors color){SetColor(Uint32(color)<<8||255);}
+    void Draw_2D::SetColor(Uint32 color){
+        memcpy(&this->color,&color,sizeof(Uint32));
+    }
+}
