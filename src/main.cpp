44cc56bbd5c7d38fe2f0a80884f4c489de373934
112590012 <t112590012@ntut.org.tw>
2025-03-07 15:08:41 +0800

    cursor

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,11 @@
 #include <iostream>
 #include "Core/Context.hpp"
 #include "Draw/Draw_2D.hpp"//test_Draw_2D
+#include "Util/Input.hpp"
 int main(int, char**) {
     auto context = Core::Context::GetInstance();
     context->SetWindowIcon(RESOURCE_DIR"/Image/assets/icon.png");
+    SDL_ShowCursor(SDL_DISABLE);
     // App app;
     // while (!context->GetExit()) {
     //     switch (app.GetCurrentState()) {
@@ -32,7 +34,8 @@ int main(int, char**) {
     // }
 
 
-    Draw::Draw_2D the_test;
+    std::shared_ptr<Draw::Draw_2D> Draw2D =std::make_shared<Draw::Draw_2D>() ;
+    
     // std::shared_ptr<Draw::ReTexture> test_image=std::make_shared<Draw::ReTexture>(RESOURCE_DIR"/Image/Start_screen/title6.png");
     //Draw::Image_Region test_region(test_image,2,61,639,550);
     std::shared_ptr<Draw::ReTexture> test_image=std::make_shared<Draw::ReTexture>(RESOURCE_DIR"/Image/cards/cards2.png");
@@ -45,12 +48,18 @@ int main(int, char**) {
         // the_test.begin();
         // the_test.draw(test_image,0,WINDOW_HEIGHT-100,100,100);
         // the_test.end();
-        initScreen.draw();
+        
+        
+        initScreen.draw(Draw2D);
+        Draw2D->draw(std::make_shared<Draw::ReTexture>(RESOURCE_DIR"/Image/cursor/gold2.png"),
+            Util::Input::GetCursorPosition().x+WINDOW_WIDTH/2-25,Util::Input::GetCursorPosition().y+WINDOW_HEIGHT/2-25,50,50);
+        Draw2D->end();
         context->Update();     
         if(initScreen.GetCurrentState()==InitScreen::State::END){
             context->SetExit(true);
         } 
     }
+    
 
     return 0;
 }
