499f820928c96a7b32a4935063c53c652b25d5ba
112590012 <t112590012@ntut.org.tw>
2025-03-19 21:07:23 +0800

    Dungeon_BottomScene

--- src/Game_object/dungeon/Dungeon_BottomScene.cpp
+++ src/Game_object/dungeon/Dungeon_BottomScene.cpp
@@ -4,10 +4,16 @@ Object::Dungeon_BottomScene::Dungeon_BottomScene()
     Background=std::make_shared<Draw::Image_Region>(RUtil::Image_book::GetTexture(RESOURCE_DIR"/image/bottomScene/scene.jpg"),2,50,1920,1136-50);
     mods.push_back(std::make_shared<Draw::Image_Region>(RUtil::Image_book::GetTexture(RESOURCE_DIR"/image/bottomScene/scene2.png"),2,402,745,572));
     mods.push_back(std::make_shared<Draw::Image_Region>(RUtil::Image_book::GetTexture(RESOURCE_DIR"/image/bottomScene/scene.png"),2,500,1920,771));
+    deckButton=std::make_shared<Draw::Image_Region>(RUtil::Image_book::GetTexture(RESOURCE_DIR"/image/deckButton/base.png"));
+    discardButton=std::make_shared<Draw::Image_Region>(RUtil::Image_book::GetTexture(RESOURCE_DIR"/image/discardButton/base.png"));
+ 
 }
 void Object::Dungeon_BottomScene::render(std::shared_ptr<Draw::Draw_2D> r2){
     r2->draw(Background, 0.0F, 0.0F, 1920*Setting::SCALE, 1180*Setting::SCALE);
     for(int i=0;i<mods.size();i++){
         r2->draw(mods[i], mods_pos[i].x, mods_pos[i].y, mods_size[i].x, mods_size[i].y);
     }
+    r2->draw(deckButton,50,25,110,110);
+    r2->draw(discardButton,1125,25,110,110);
+    
 }
